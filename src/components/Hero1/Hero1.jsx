dde72e09698232bbef18f8e5e24ba9a866c96998
Juan Cruz <juancruzbonadeo04@gmail.com>
2024-04-05 11:52:41 -0300

    responsive design

--- src/components/Hero1/Hero1.jsx
+++ src/components/Hero1/Hero1.jsx
@@ -5,7 +5,7 @@ import Button from '../Button/Button';
 
 const Hero1 = () =>{
     return (
-      <Carousel interval={5000}>
+      <Carousel interval={5000000}>
         <Carousel.Item className='Hero1Container'>
           <img
             className="imgHero1"
@@ -13,20 +13,20 @@ const Hero1 = () =>{
             alt="First-slide"
           />
           <Carousel.Caption className='HeroCaption'>
-            <Button label='Ver Productos' to='/productos' />
+            <Button label='Comprar' to='/productos' />
           </Carousel.Caption>
         </Carousel.Item> 
         
-        {/* <Carousel.Item className='Hero1Container hero2'>
+        <Carousel.Item className='Hero1Container'>
           <img
             className="imgHero1"
-            src='https://firebasestorage.googleapis.com/v0/b/termomelga.appspot.com/o/heros%2Fhero1.jpg?alt=media&token=73ac345e-bced-4dbc-a249-8dddd6877e17'
+            src='https://firebasestorage.googleapis.com/v0/b/termomelga.appspot.com/o/heros%2Fhero1.jpg?alt=media&token=a284a4d1-5920-4a8a-92d2-c72c868a86a2'
             alt="Second-slide"
           />
-          <Carousel.Caption className='HeroCaption'>
+          <Carousel.Caption className='HeroCaption hero3'>
             <Button label='Ver Mates' to='/categoria/mates'  />
           </Carousel.Caption>
-        </Carousel.Item>  */}
+        </Carousel.Item>
 
         <Carousel.Item className='Hero1Container'>
           <img
@@ -34,7 +34,7 @@ const Hero1 = () =>{
             src='https://firebasestorage.googleapis.com/v0/b/termomelga.appspot.com/o/heros%2Fhero2black.jpg?alt=media&token=197fcb10-b6bd-4afa-86eb-d56a98fbbac2'
             alt="Third-slide"
           />
-          <Carousel.Caption className='HeroCaption'>
+          <Carousel.Caption className='HeroCaption '>
             <Button label='Ver Yerbas' to='/categoria/yerbas'  />
           </Carousel.Caption>
         </Carousel.Item> 
