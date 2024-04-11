be68c57fafcf2c3df765d6da24241c53c8078d09
Juan Cruz <juancruzbonadeo04@gmail.com>
2024-04-11 10:07:58 -0300

    sape

--- src/components/Hero1/Hero1.jsx
+++ src/components/Hero1/Hero1.jsx
@@ -3,43 +3,44 @@ import Carousel from 'react-bootstrap/Carousel';
 import Button from '../Button/Button';
 
 
-const Hero1 = () =>{
-    return (
-    <Carousel interval={10000}>
-        <Carousel.Item className='Hero1Container'>
-          <img
-            className="imgHero1 imgHero3"
-            src='https://firebasestorage.googleapis.com/v0/b/termomelga.appspot.com/o/heros%2Fhero2Mobile.jpg?alt=media&token=4cb2bbe6-1b10-4e1b-a40e-31911921aa98'
-            alt="Third-slide"
-          />
-          <Carousel.Caption className='HeroCaption hero12'>
-            <Button label='Ver Yerbas' to='/categoria/yerbas'  />
-          </Carousel.Caption>
-        </Carousel.Item> 
-      
-      
-        <Carousel.Item className='Hero1Container'>
-          <img
-            className="imgHero1"
-            src='https://firebasestorage.googleapis.com/v0/b/termomelga.appspot.com/o/heros%2Fhero1Mobile.jpg?alt=media&token=464481d1-1d29-44aa-8bcf-f5a17a24ea7b'
-            alt="First-slide"
-          />
-          <Carousel.Caption className='HeroCaption hero12'>
-            <Button label='Comprar' to='/productos' />
-          </Carousel.Caption>
-        </Carousel.Item> 
-        
-        <Carousel.Item className='Hero1Container'>
-          <img
-            className="imgHero1"
-            src='https://firebasestorage.googleapis.com/v0/b/termomelga.appspot.com/o/heros%2Fhero1.jpg?alt=media&token=403b9243-1506-4708-8c4e-bb1acc5b3a4c'
-            alt="Second-slide"
-          />
-          <Carousel.Caption className='HeroCaption hero3'>
-            <Button label='Ver Mates' to='/categoria/mates'  />
-          </Carousel.Caption>
-        </Carousel.Item>
-</Carousel>
-    );
+const Hero1 = () => {
+  return (
+    <Carousel interval={5000}>
+
+
+      <Carousel.Item className='Hero1Container'>
+        <img
+          className="imgHero1"
+          src='https://firebasestorage.googleapis.com/v0/b/termomelga.appspot.com/o/heros%2Fhero1.jpg?alt=media&token=0bb247f1-2ce9-431e-9fe7-b1dc2d8237ab'
+          alt="First-slide"
+        />
+        <Carousel.Caption className='HeroCaption hero12'>
+          <Button label='Comprar' to='/productos' />
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item className='Hero1Container'>
+        <img
+          className="imgHero1 imgHero3"
+          src='https://firebasestorage.googleapis.com/v0/b/termomelga.appspot.com/o/heros%2Fhero2.jpg?alt=media&token=00d11485-5ebd-4c96-b442-9b5c6ba2e59d'
+          alt="Third-slide"
+        />
+        <Carousel.Caption className='HeroCaption hero12'>
+          <Button label='Ver Yerbas' to='/categoria/yerbas' />
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item className='Hero1Container'>
+        <img
+          className="imgHero1"
+          src='https://firebasestorage.googleapis.com/v0/b/termomelga.appspot.com/o/heros%2Fhero3.jpg?alt=media&token=9e27c755-c246-4b86-b8d9-25b2d4401825'
+          alt="Second-slide"
+        />
+        <Carousel.Caption className='HeroCaption hero3'>
+          <Button label='Ver Mates' to='/categoria/mates' />
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
 }
 export default Hero1
--- src/components/Hero2/Hero2.jsx
+++ src/components/Hero2/Hero2.jsx
@@ -4,7 +4,7 @@ import { useInView } from 'react-intersection-observer';
 import Button from '../Button/Button';
 import '../Hero2/hero2.css';
 
- export const Hero2 = () => {
+export const Hero2 = () => {
   const [isVisible, setIsVisible] = React.useState(false);
   const { ref, inView } = useInView({
     threshold: 0.5, // Cambia el valor según tus necesidades
@@ -21,19 +21,19 @@ import '../Hero2/hero2.css';
     <motion.div
       className="CombosContainer"
       ref={ref}
-      initial={{ opacity: 0, scale: 0}}
+      initial={{ opacity: 0, scale: 0 }}
       animate={{ opacity: isVisible ? 1 : 0, scale: isVisible ? 1 : 0 }}
-      
-      transition={{ duration: 4, delay: 1,type:"spring", stiffness: 160, damping: 20}}
+
+      transition={{ duration: 4, delay: 1, type: "spring", stiffness: 160, damping: 20 }}
     >
       <img
-        src='https://firebasestorage.googleapis.com/v0/b/termomelga.appspot.com/o/heros%2Fcombos.jpg?alt=media&token=45a636ef-aacb-47d5-af51-453e0d17176a'
+        src='https://firebasestorage.googleapis.com/v0/b/termomelga.appspot.com/o/heros%2Fbanner2.jpeg?alt=media&token=ad7d9f4a-57ff-4b60-99ee-703055094226'
         alt='Combos'
         className='combosImg'
       />
       <div className="info">
-        <h2>Combos Materos</h2>
-        <p>¡Disfrutá del auténtico mate argentino en cualquier momento con nuestros combos materos! Incluye mate, bombilla y termo de calidad. ¡No te pierdas esta tradición única!
+        <h2>Yerbas Importadas🌿</h2>
+        <p>Vendemos las mejores yerbas importadas. ¡No te las pierdas y disfruta de un buen mate!
         </p>
         <Button label='Ver Combos' to='/categoria/combos' />
       </div>
