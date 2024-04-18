5d4ee3beaf394b4f74e25fb3955c4a8343016c40
Juan Cruz <juancruzbonadeo04@gmail.com>
2024-04-18 11:36:42 -0300

    admin logic

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,36 +1,93 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import './Admin.css'
-import { collection, addDoc, setDoc, doc} from 'firebase/firestore'
+import { collection, addDoc, setDoc, doc } from 'firebase/firestore'
+import { ref, uploadBytesResumable, getDownloadURL, listAll, list } from "firebase/storage";
 import { db } from '../../services/firebase/firebaseConfig'
+import { storage } from '../../services/firebase/firebaseConfig'
 import Swal from 'sweetalert2';
 
 
 export const Admin = () => {
-    const addProduct = (e) => {
+    const [image1, setImg1] = useState(null);
+    const [image2, setImg2] = useState(null);
+    const [imagesListRef, setImagesListRef] = useState(ref(storage, 'Productos/mates'));
+    const [category, setCategory] = useState('mates');
+    const [nombre, setNombre] = useState('');
+    const [imagePreview1, setImagePreview1] = useState('');
+    const [imagePreview2, setImagePreview2] = useState('');
+
+    const handleNombre = (e) => {
+        setNombre(e.target.value);
+    }
+
+    const handleCategory = (e) => {
+        const selectedCategory = e.target.value;
+        setCategory(selectedCategory);
+        setImagesListRef(ref(storage, `Productos/${selectedCategory}`));
+    }
+
+    const handleImage1Change = (e) => {
+        const file = e.target.files[0];
+        setImg1(file);
+        // Mostrar la vista previa de la imagen
+        const reader = new FileReader();
+        reader.onload = () => {
+            setImagePreview1(reader.result);
+        };
+        reader.readAsDataURL(file);
+    }
+
+    const handleImage2Change = (e) => {
+        const file = e.target.files[0];
+        setImg2(file);
+        // Mostrar la vista previa de la imagen
+        const reader = new FileReader();
+        reader.onload = () => {
+            setImagePreview2(reader.result);
+        };
+        reader.readAsDataURL(file);
+    }
+
+    const addProduct = async (e) => {
         e.preventDefault();
-        const nombre = document.getElementById('nombre').value;
-        const precio = document.getElementById('precio').value;
-        const descuento = document.getElementById('descuento').value;
+        // Crear una referencia a la carpeta del producto
+        const productFolderRef = ref(storage, `productos/${nombre}`);
+
+        // Subir la primera imagen
+        let imageUrl1 = '';
+        if (image1) {
+            const image1Ref = ref(productFolderRef, image1.name);
+            await uploadBytesResumable(image1Ref, image1);
+            imageUrl1 = await getDownloadURL(image1Ref);
+        }
+        
+        // Subir la segunda imagen
+        let imageUrl2 = '';
+        if (image2) {
+            const image2Ref = ref(productFolderRef, image2.name);
+            await uploadBytesResumable(image2Ref, image2);
+            imageUrl2 = await getDownloadURL(image2Ref);
+        }
+        const precio = parseInt(document.getElementById('precio').value)
+        const descuento = parseInt(document.getElementById('descuento').value)
         const stock = document.getElementById('stock').checked;
-        const category = document.getElementById('category').value;
         const descripcion = document.getElementById('descripcion').value;
-        const img1 = document.getElementById('img1').value;
-        const img2 = document.getElementById('img2').value;
-        const nombreProducto = nombre.toUpperCase().replace(/\s+/g, '-');
+        const nombreProducto = nombre.replace(/\s+/g, '-');
         const nuevoProducto = {
             nombre: nombre,
             precio: precio,
             descuento: descuento,
             stock: stock,
-            category: category,
+            categoria: category,
             descripcion: descripcion,
-            img1: img1,
-            img2: img2
+            img1: imageUrl1,
+            img2:  imageUrl2
         }
-         // Replace 'your-desired-id' with the desired ID for the document
+        // Replace 'your-desired-id' with the desired ID for the document
         const productRef = doc(db, 'products', nombreProducto);
         setDoc(productRef, nuevoProducto)
-        
+
             .then(() => {
                 Swal.fire({
                     icon: 'success',
@@ -47,7 +104,7 @@ export const Admin = () => {
                     timer: 1500
                 })
             })
-        
+
     }
     return (
         <div className="adminContainer">
@@ -55,9 +112,9 @@ export const Admin = () => {
                 <h1>Administrar Productos</h1>
                 <div className="form-group">
                     <label htmlFor="name">Nombre:</label>
-                    <input type="text" id="nombre" name="nombre" required />
+                    <input type="text" id="nombre" name="nombre" required value={nombre} onChange={handleNombre} />
                     <label htmlFor="stock">Stock:</label>
-                    <input type="checkbox" id="stock" name="stock"  className='stock'/>
+                    <input type="checkbox" id="stock" name="stock" className='stock' />
                 </div>
                 <div className="form-group">
                     <label htmlFor="price">Precio:</label>
@@ -78,12 +135,12 @@ export const Admin = () => {
                         <option value="60">60%</option>
                     </select>
                 </div>
-                
+
                 <div className="form-group">
                     <label htmlFor="descripcion">Descripcion:</label>
                     <input type="text" id="descripcion" name="descripcion" />
                     <label htmlFor="category">Categoría:</label>
-                    <select name="category" id="category" required>
+                    <select name="category" id="category" required value={category} onChange={handleCategory}>
                         <option value="mates">Mates</option>
                         <option value="yerbas">Yerbas</option>
                         <option value="bombillas">Bombillas</option>
@@ -91,14 +148,16 @@ export const Admin = () => {
                         <option value="materas">Materas</option>
                         <option value="combos">Combos</option>
                     </select>
+
                 </div>
-                <div className="form-group">
-                    <label htmlFor="imagen">Imagen 1:</label>
-                    <input type="text" id="img1" name="imagen1" />
-                    <label htmlFor="imagen">Imagen 2:</label>
-                    <input type="text" id="img2" name="imagen2" />
-                </div>
+                <label htmlFor="imagen">Imagen 1:</label>
+                <input type="file" id="img1" name="imagen1" onChange={handleImage1Change}/>
+                {imagePreview1 && <img src={imagePreview1} alt="Preview" style={{ maxWidth: '100px' }} />}
+                <label htmlFor="imagen">Imagen 2:</label>
+                <input type="file" id="img2" name="imagen2"  onChange={handleImage2Change}/>
+                {imagePreview2 && <img src={imagePreview2} alt="Preview" style={{ maxWidth: '100px' }} />}
                 <button className="Button" type='submit'>Agregar</button>
+                
             </form>
         </div>
     )
